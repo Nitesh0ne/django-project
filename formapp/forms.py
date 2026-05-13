fce0e8a42fd650814f03b1822cfe4050acb2379e
nitesh <pingnitesh07@gmail.com>
2026-05-13 20:40:49 +0545

    session 5 form validation

--- formapp/forms.py
+++ formapp/forms.py
@@ -1,9 +1,24 @@
 from django import forms
+from django.core import validators
+
+
+def starts_with_v(value):
+    if value[0].lower() != 'v':
+        raise forms.ValidationError("Name should start with v. ")
+    
+
+
 
 class EmployeeForm(forms.Form):
-    ename = forms.CharField()
+    ename = forms.CharField(validators=[starts_with_v]) 
     eaddr = forms.CharField()
     eno = forms.IntegerField()
     esal = forms.FloatField()
 
-    
+    # validation done by programmer using clean method 
+    # def clean_ename(self):
+    #     input_name = self.cleaned_data['ename']
+    #     if len(input_name) < 5 :
+    #         raise  forms.ValidationError("The minimum number of employee name should be greater than 5")
+    #     return input_name
+
