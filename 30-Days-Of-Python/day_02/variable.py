44f05b40842004c51a02330d54b29fa94b33af51
Nitesh Nepali <Nitesh0ne>
2026-05-14 23:06:10 +0545

    update

--- 30-Days-Of-Python/day_02/variable.py
+++ 30-Days-Of-Python/day_02/variable.py
@@ -1,4 +1,57 @@
+#Day 2: 30 Days of python programming
+
+f_name = "Nitesh"
+l_name = "Nepali"
+full_name = "Nitesh Nepali"
+country = "Nepal"
+city  = "Pokhara"
+age = 20
+year = 2060
+is_married = False
+is_true = False
+is_light = True 
+
+a, b, c = 0, 1, 2
 
 
+#######################
+#Excersie 2 
 
+check_data_type=type(f_name)
+print("The data type of f_name",check_data_type)
+
+check_data_type=type(l_name)
+print("The data type of l_name", check_data_type)
+
+check_data_type=type(full_name)
+print("The data type of full_name", check_data_type)
+
+check_data_type=type(country)
+print("The data type of country", check_data_type)
+
+check_data_type=type(city)
+print("The data type of city", check_data_type)
+
+check_data_type=type(age)
+print("The data type of age", check_data_type)
+
+check_data_type=type(year)
+print("The data type of year", check_data_type)
+
+check_data_type=type(is_married)
+print("The data type of is_married", check_data_type)
+
+check_data_type=type(is_true)
+print("The data type of is_true", check_data_type)
+
+check_data_type=type(is_light)
+print("The data type of is_light", check_data_type)
+
+# print(dir(str))
+# print(dir(bool))
+len_fname= len(f_name)
+print("The length of the f_name is: ", len_fname)
+
+len_lname= len(l_name)
+print("The lenght of the l_name is:", len_lname)
 
