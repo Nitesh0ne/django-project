99f45d267cef42b7f775892efd2989a2e322716c
nitesh <pingnitesh07@gmail.com>
2026-05-14 15:50:42 +0545

    30 days of pyhton added into folder

---
+++ 30-Days-Of-Python/01_Day_Introduction/helloworld.py
@@ -0,0 +1,34 @@
+# Introduction
+# Day 1 - 30DaysOfPython Challenge
+
+print("Hello World!")   # print hello world
+
+print("Additiion : ", 2 + 3)   # addition(+)
+print("Subtratction : ", 3 - 1)   # subtraction(-)
+print("Multiplication : ",2 * 3)   # multiplication(*)
+print(3 + 2)   # addition(+)
+print(3 - 2)   # subtraction(-)
+print(3 * 2)   # multiplication(*)
+print("Division",10/3)   # division(/)
+print( 3 ** 2)  # exponential(**)
+print("Module",10 % 2)   # modulus(%)
+print(3 // 2)  # Floor division operator(//)
+
+# Checking data types
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('Asabeneh'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'name': 'Asabeneh'}))  # Dictionary
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
+
+my_dict = {
+    'f_name': "Nitesh",
+    'l_name' : "Nepali"
+}
+
+print(my_dict["f_name"])
+print(type(my_dict))
--- practiceproject/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PracticeprojectConfig(AppConfig):
-    name = 'practiceproject'
